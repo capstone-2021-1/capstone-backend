cf1018d916d1c2eeafc4309d8ed5ec01874f2c1b
nick_kim <sowhat443@gmail.com>
2021-05-30 19:03:44 +0900

    중복제거

--- src/main/java/com/example/demo/src/user/UserController.java
+++ src/main/java/com/example/demo/src/user/UserController.java
@@ -47,6 +47,7 @@ public class UserController {
     @PostMapping("/signin")
     public BaseResponse<PostUserRes> createUser(@RequestBody PostUserReq postUserReq) {
         // TODO: email 관련한 짧은 validation 예시입니다. 그 외 더 부가적으로 추가해주세요!
+        // 미기입
         if(postUserReq.getEmail() == null){
             return new BaseResponse<>(POST_USERS_EMPTY_EMAIL);
         }
@@ -54,9 +55,7 @@ public class UserController {
         if(!isRegexEmail(postUserReq.getEmail())){
             return new BaseResponse<>(POST_USERS_INVALID_EMAIL);
         }
-        if(postUserReq.getEmail() == null){
-            return new BaseResponse<>(POST_USERS_EMPTY_EMAIL);
-        }
+
         try{
             PostUserRes postUserRes = userService.createUser(postUserReq);
             return new BaseResponse<>(postUserRes);
